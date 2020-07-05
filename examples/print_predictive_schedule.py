2e0de404177a8dfb35a85cecfed61f67f544a945
Jumper78 <52802286+Jumper78@users.noreply.github.com>
2020-07-05 16:53:02 +0200

    added also the printing of the scheduled days

--- examples/print_predictive_schedule.py
+++ examples/print_predictive_schedule.py
@@ -8,11 +8,20 @@ from pyIndego import IndegoClient
 def main(config):
     with IndegoClient(**config) as indego:
         indego.update_predictive_schedule()
+        
+        print("Times where SmartMowing is planing to mow the lawn:")
+        
+        for i in range(len(indego.predictive_schedule.schedule_days)):
+            print("\t{}".format(indego.predictive_schedule.schedule_days[i].day_name))
+            for j in range(len(indego.predictive_schedule.schedule_days[i].slots)):
+                print('\t\t{:%H:%M} - {:%H:%M}'.format(indego.predictive_schedule.schedule_days[i].slots[j].start, indego.predictive_schedule.schedule_days[i].slots[j].end))
 
+        print("Times that are excluded for mowing from SmartMowing:")
+        
         for i in map(lambda x : x + datetime.now().weekday(), range(len(indego.predictive_schedule.exclusion_days))):
-            print(indego.predictive_schedule.exclusion_days[i % 7].day_name)
+            print("\t{}".format(indego.predictive_schedule.exclusion_days[i % 7].day_name))
             for j in range(len(indego.predictive_schedule.exclusion_days[i % 7].slots)):
-                print('\t{:%H:%M} - {:%H:%M} {}'.format(indego.predictive_schedule.exclusion_days[i % 7].slots[j].start, indego.predictive_schedule.exclusion_days[i % 7].slots[j].end, indego.predictive_schedule.exclusion_days[i % 7].slots[j].Attr))
+                print('\t\t{:%H:%M} - {:%H:%M} {}'.format(indego.predictive_schedule.exclusion_days[i % 7].slots[j].start, indego.predictive_schedule.exclusion_days[i % 7].slots[j].end, indego.predictive_schedule.exclusion_days[i % 7].slots[j].Attr))
 
 if __name__ == "__main__":
     with open("config.json", "r") as config_file:
