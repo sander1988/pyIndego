ed295264b8ee2f31f1b80fc5074d226f8ef9d895
Jumper78 <52802286+Jumper78@users.noreply.github.com>
2020-07-05 20:36:26 +0200

    streamlined loops to get the weekdays into the right order starting with today

--- examples/print_predictive_schedule.py
+++ examples/print_predictive_schedule.py
@@ -11,17 +11,21 @@ def main(config):
         
         print("Times where SmartMowing is planing to mow the lawn:")
         
-        for i in range(len(indego.predictive_schedule.schedule_days)):
-            print("\t{}".format(indego.predictive_schedule.schedule_days[i].day_name))
-            for j in range(len(indego.predictive_schedule.schedule_days[i].slots)):
-                print('\t\t{:%H:%M} - {:%H:%M}'.format(indego.predictive_schedule.schedule_days[i].slots[j].start, indego.predictive_schedule.schedule_days[i].slots[j].end))
-
+        for i in range(datetime.now().weekday(), datetime.now().weekday()+7):
+            for j in range(len(indego.predictive_schedule.schedule_days)):
+                if (indego.predictive_schedule.schedule_days[j].day == (i % 7)):
+                    print("\t{}".format(indego.predictive_schedule.schedule_days[j].day_name))
+                    for k in range(len(indego.predictive_schedule.schedule_days[j].slots)):
+                        print('\t\t{:%H:%M} - {:%H:%M}'.format(indego.predictive_schedule.schedule_days[j].slots[k].start, indego.predictive_schedule.schedule_days[j].slots[k].end))
+        
         print("Times that are excluded for mowing from SmartMowing:")
         
-        for i in map(lambda x : x + datetime.now().weekday(), range(len(indego.predictive_schedule.exclusion_days))):
-            print("\t{}".format(indego.predictive_schedule.exclusion_days[i % 7].day_name))
-            for j in range(len(indego.predictive_schedule.exclusion_days[i % 7].slots)):
-                print('\t\t{:%H:%M} - {:%H:%M} {}'.format(indego.predictive_schedule.exclusion_days[i % 7].slots[j].start, indego.predictive_schedule.exclusion_days[i % 7].slots[j].end, indego.predictive_schedule.exclusion_days[i % 7].slots[j].Attr))
+        for i in range(datetime.now().weekday(), datetime.now().weekday()+7):
+            for j in range(len(indego.predictive_schedule.exclusion_days)):
+                if (indego.predictive_schedule.exclusion_days[j].day == (i % 7)):
+                    print("\t{}".format(indego.predictive_schedule.exclusion_days[j].day_name))
+                    for k in range(len(indego.predictive_schedule.exclusion_days[j].slots)):
+                        print('\t\t{:%H:%M} - {:%H:%M} {}'.format(indego.predictive_schedule.exclusion_days[j].slots[k].start, indego.predictive_schedule.exclusion_days[j].slots[k].end, indego.predictive_schedule.exclusion_days[j].slots[k].Attr))
 
 if __name__ == "__main__":
     with open("config.json", "r") as config_file:
